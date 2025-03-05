937db8420ca4ff89e18f4565158b1eb957ae5609
Admin <Admin@example.com>
2025-03-05 16:58:06 +0100

    fix for new main class causing issues with scanning components by folder tree level

--- react-ui/vite.config.ts
+++ react-ui/vite.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import {viteEnvs} from "vite-envs";
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    viteEnvs()
   ],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 8081,  // Specify the port where your app is running
+  },
   // define: {
   //   'import.meta.env.VITE_BACKEND_URL': JSON.stringify('http://localhost:8081')
   // }
